2d33d17e98c7ce6c6c47f6d86531e726ef1a858e
janghona <jang1na2@gmail.com>
2021-08-29 16:05:54 +0900

    IOCP echo 서버 커밋
    * 기본 TCP 에코서버에서 IOCP 서버로 변경
    * 클래스화하여 다형성을 좀 더 확장 시킴

---
+++ Jangho_Server/main.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "IOCompletionPort.h"
+
+int main()
+{
+	IOCompletionPort iocp_server;
+	if (iocp_server.Initialize()){
+		iocp_server.StartServer();
+	}
+	return 0;
+}
+
